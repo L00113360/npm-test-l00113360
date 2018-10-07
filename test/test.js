f2bafca17dea9162727b00039930060568e681fb
L00113360 <33516120+L00113360@users.noreply.github.com>
2018-10-07 02:11:24 +0100

    Added Mocha & Chai

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var expect = require('chai').expect;
+
+describe('Testing with Mocha', function () {
+    it('should pass when everything is good', function () {
+        expect(true).to.be.true;
+    })
+})
